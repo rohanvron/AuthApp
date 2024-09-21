46470545d61b8f59b2fc14ecc3d13a219b8d4c9c
rohanvron <164373071+rohanvron@users.noreply.github.com>
2024-09-22 01:55:04 +0530

    vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/',
   plugins: [react()],
-  base: '/AuthApp/',
   server: {
     proxy: {
       '/api': 'https://auth-app-wine-one.vercel.app'
